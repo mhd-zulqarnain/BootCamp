817868fed1bac673cefd7996a79c09242220bee4
panacloud <mhd.zeeq@gmail.com>
2018-01-23 16:41:46 +0500

    Facebook-google-twitter-authentication

--- AuthenticationDemo/app/src/main/java/com/example/zulup/authenticationdemo/MainActivity.java
+++ AuthenticationDemo/app/src/main/java/com/example/zulup/authenticationdemo/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.zulup.authenticationdemo;
 
-import android.app.Application;
 import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
@@ -16,39 +15,54 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.zulup.authenticationdemo.utils.Messege;
 import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
-import com.facebook.FacebookSdk;
 import com.facebook.GraphRequest;
 import com.facebook.GraphResponse;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
-import com.facebook.login.widget.LoginButton;
 
-import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.Task;
+import com.twitter.sdk.android.core.Callback;
+import com.twitter.sdk.android.core.DefaultLogger;
+import com.twitter.sdk.android.core.Result;
 import com.twitter.sdk.android.core.Twitter;
+import com.twitter.sdk.android.core.TwitterApiClient;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import com.twitter.sdk.android.core.TwitterConfig;
+import com.twitter.sdk.android.core.TwitterCore;
+import com.twitter.sdk.android.core.TwitterException;
+import com.twitter.sdk.android.core.TwitterSession;
+import com.twitter.sdk.android.core.identity.TwitterAuthClient;
+import com.twitter.sdk.android.core.models.User;
+import com.twitter.sdk.android.core.services.AccountService;
 
-import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 
+import retrofit2.Call;
+
 public class MainActivity extends AppCompatActivity {
 
     CallbackManager callbackManager;
-    Button CustomLoginButton;
+    Button CustomFacebookLoginButton;
+    Button CustomTwitterLoginButton;
     TextView textView;
-    final private int RC_GOOGLE = 90;
+    TwitterAuthClient mTwitterAuthClient;
+      private int RC_GOOGLE = 90;
+    private Bundle args;
+
     private GoogleSignInClient mGoogleSignInClient;
 
     @Override
@@ -56,12 +70,74 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Twitter.initialize(this);
+        args = new Bundle();
+
         textView = findViewById(R.id.result_facebook);
         callbackManager = CallbackManager.Factory.create();
-        CustomLoginButton = findViewById(R.id.login_button);
+        CustomFacebookLoginButton = findViewById(R.id.login_button);
+        CustomTwitterLoginButton = findViewById(R.id.twt_login_button);
+        mTwitterAuthClient = new TwitterAuthClient();
+
+
+        loginWithTwt();
         loginWithFb();
     }
 
+    private void loginWithTwt() {
+
+        TwitterConfig config = new TwitterConfig.Builder(this)
+                .logger(new DefaultLogger(Log.DEBUG))
+                .twitterAuthConfig(new TwitterAuthConfig("Wi5dqnFtlbykhUbHYmyaJzyUM",
+                        "9mxOThtOCUNs9N6sqIkKf57e7kFpI34xHlAKxeefKFt9DWqC3w"))
+                .debug(true)
+                .build();
+        Twitter.initialize(config);
+
+        CustomTwitterLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mTwitterAuthClient.authorize(MainActivity.this, new Callback<TwitterSession>() {
+                    @Override
+                    public void success(Result<TwitterSession> result) {
+                        TwitterApiClient twitterApiClient = TwitterCore.getInstance().getApiClient();
+                        AccountService accountService = twitterApiClient.getAccountService();
+
+                        Call<User> call = accountService.verifyCredentials(true, true, true);
+                        call.enqueue(new Callback<com.twitter.sdk.android.core.models.User>() {
+                            @Override
+                            public void success(Result<com.twitter.sdk.android.core.models.User> result) {
+
+
+
+                                args.putString("username", result.data.name.toString());
+                                args.putString("img_url", result.data.profileImageUrl.toString());
+
+                                TwitterHomeFragment  homeFragment = new TwitterHomeFragment();
+                                homeFragment.setArguments(args);
+                                FragmentTransaction ftransaction= getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, homeFragment);
+                                ftransaction.commit();
+                            }
+
+                            @Override
+                            public void failure(TwitterException exception) {
+                                Messege.messege(MainActivity.this, "inner exxeption");
+
+                            }
+                        });
+
+
+                    }
+
+                    @Override
+                    public void failure(TwitterException exception) {
+                        Messege.messege(MainActivity.this, "Exception" + exception);
+                        Log.d("", "failure: " + exception);
+                    }
+                });
+            }
+        });
+
+    }
 
     private void loginWithFb() {
 
@@ -83,7 +159,7 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-        CustomLoginButton.setOnClickListener(new View.OnClickListener() {
+        CustomFacebookLoginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 LoginManager.getInstance().logInWithReadPermissions(MainActivity.this, Arrays.asList("public_profile", "email", "user_friends"));
@@ -124,7 +200,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         callbackManager.onActivityResult(requestCode, resultCode, data);
-
+        mTwitterAuthClient.onActivityResult(requestCode, resultCode, data);
         if (requestCode == RC_GOOGLE) {
             Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
             handleSignInResult(task);
@@ -137,33 +213,32 @@ public class MainActivity extends AppCompatActivity {
         try {
             GoogleSignInAccount account = task.getResult(ApiException.class);
 
-           Toast.makeText(getApplicationContext(),account.getEmail()+""+account.getDisplayName(),Toast.LENGTH_SHORT).show();
+            Toast.makeText(getApplicationContext(), account.getEmail() + "" + account.getDisplayName(), Toast.LENGTH_SHORT).show();
         } catch (Exception e) {
             e.printStackTrace();
-            Log.d("", "handleSignInResult:Exception "+e);
+            Log.d("", "handleSignInResult:Exception " + e);
         }
     }
 
     public void graphRequest(AccessToken token) {
-        final Bundle args = new Bundle();
+
         GraphRequest request = GraphRequest.newMeRequest(token, new GraphRequest.GraphJSONObjectCallback() {
 
             @Override
             public void onCompleted(JSONObject object, GraphResponse response) {
 
                 try {
-//                    textView.setText(object.toString());
-                    args.putString("first_name", object.getString("first_name"));
-                    args.putString("last_name", object.getString("last_name"));
+//                    textView.setText(object.toString());\
+                    args.putString("username", object.getString("first_name"));
+                    args.putString("img_url", object.getJSONObject("picture").getJSONObject("data").getString("url"));
 
-                    HomeFragment homeFragment = new HomeFragment();
+                    FacebookHomeFragment homeFragment = new FacebookHomeFragment();
                     homeFragment.setArguments(args);
-                    FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-                    transaction.replace(R.id.fragment_container, homeFragment);
+                    FragmentTransaction transaction=getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, homeFragment);
                     transaction.commit();
 
                 } catch (Exception e) {
-                    Toast.makeText(getApplicationContext(), "Exception", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getApplicationContext(), "Exception" + e, Toast.LENGTH_SHORT).show();
                 }
 
             }
--- AuthenticationDemo/app/src/main/java/com/example/zulup/authenticationdemo/TwitterHomeFragment.java
+++ AuthenticationDemo/app/src/main/java/com/example/zulup/authenticationdemo/TwitterHomeFragment.java
@@ -3,7 +3,6 @@ package com.example.zulup.authenticationdemo;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,28 +10,30 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.facebook.AccessToken;
-import com.facebook.GraphRequest;
-import com.facebook.login.LoginManager;
+
+import com.squareup.picasso.Picasso;
+import com.twitter.sdk.android.core.Twitter;
+import com.twitter.sdk.android.core.TwitterCore;
+import com.twitter.sdk.android.core.TwitterSession;
+
 
 /**
  * Created by zulup on 1/22/2018.
  */
 
-public class HomeFragment extends Fragment {
+public class TwitterHomeFragment extends Fragment {
     private TextView textView;
     private Button btnLogout;
-    String firstName;
-    String lastName;
+    private ImageView imageView;
+    String userName;
+    String imgUrl;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Bundle bundle = getArguments();
-        firstName = bundle.getString("first_name");
-        lastName = bundle.getString("last_name");
-        Log.d("", "onCreate: " + firstName);
-        Log.d("", "onCreate first: " + lastName);
+        userName = bundle.getString("username");
+        imgUrl = bundle.getString("img_url");
 
     }
 
@@ -42,8 +43,10 @@ public class HomeFragment extends Fragment {
         View v = LayoutInflater.from(container.getContext()).inflate(R.layout.home_fragement_view, container, false);
         textView = v.findViewById(R.id.text_response);
         btnLogout = v.findViewById(R.id.fb_logout_btn);
+        imageView = v.findViewById(R.id.prf_img);
 //        imageView.setImageBitmap();
-        textView.setText(firstName + " " + lastName);
+        textView.setText(userName);
+        Picasso.with(container.getContext()).load(imgUrl).into(imageView);
 
         logout();
 
@@ -55,11 +58,10 @@ public class HomeFragment extends Fragment {
             @Override
             public void onClick(View view) {
 
-                if (AccessToken.getCurrentAccessToken() == null) {
-                    textView.setText("No user found");
-                }
-                LoginManager.getInstance().logOut();
-                textView.setText("logout");
+                TwitterCore.getInstance().getSessionManager().clearActiveSession();
+                imageView.setImageBitmap(null);
+                textView.setText("Session Destroy");
+
             }
         });
     }
