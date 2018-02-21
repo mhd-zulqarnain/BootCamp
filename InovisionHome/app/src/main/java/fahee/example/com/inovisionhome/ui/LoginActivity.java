94c41615a4c5b5d6bb5da8ea91d0abb59b484df6
faizrehman1 <muhammad_imran01@hotmail.com>
2018-02-21 17:57:16 +0500

    inovision-ui-check

--- InovisionHome/app/src/main/java/fahee/example/com/inovisionhome/ui/LoginActivity.java
+++ InovisionHome/app/src/main/java/fahee/example/com/inovisionhome/ui/LoginActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 
 import fahee.example.com.inovisionhome.R;
 
@@ -13,6 +14,8 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
 
 
--- InovisionHome/app/src/main/java/fahee/example/com/inovisionhome/ui/SignupActivity.java
+++ InovisionHome/app/src/main/java/fahee/example/com/inovisionhome/ui/SignupActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 
 import fahee.example.com.inovisionhome.R;
 
@@ -13,6 +14,8 @@ public class SignupActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
     public void login(View v){
         Intent intent = new Intent(SignupActivity.this,LoginActivity.class);
